f464600efce31071b8425645e244258c92180c21
IhorShylov <shilov0209@gmail.com>
2022-04-14 21:52:07 +0300

    learning

---
+++ hello_world/app.js
@@ -0,0 +1,18 @@
+// Variables
+// CamelCase
+const firstName = 'Ihor';
+const lastName = 'Shylov'; // string
+const age = 31; // number
+const isProgrammer = true; // boolean
+
+const _ = 'private';
+const $ = 'some value';
+
+// const if = 'err'; // error
+// const 5withNum = 5; // error
+const withNum5 = '5';
+
+// 2 Mutation
+console.log('Name: ' + firstName + ', age: ' + age);
+console.log(age);
+
