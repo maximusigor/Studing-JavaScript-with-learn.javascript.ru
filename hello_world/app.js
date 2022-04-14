99b30d77ddce6f10b558cb79b431964b4e9d8134
IhorShylov <shilov0209@gmail.com>
2022-04-14 22:32:40 +0300

    learning

--- hello_world/app.js
+++ hello_world/app.js
@@ -1,22 +1,68 @@
-// Variables
-// CamelCase
-const firstName = 'Ihor';
-//const lastName = 'Shylov'; // string
-const age = 31; // number
-const isProgrammer = true; // boolean
+// 1 Variables
 
-const _ = 'private';
-const $ = 'some value';
+
+// CamelCase
+//const firstName = 'Ihor';
+//const lastName = 'Shylov'; // string
+//const age = 31; // number
+// const isProgrammer = true; // boolean
+//
+// const _ = 'private';
+// const $ = 'some value';
 
 // const if = 'err'; // error
 // const 5withNum = 5; // error
-const withNum5 = '5';
+//const withNum5 = '5';
 
 // 2 Mutation
+
+
 //console.log('Name: ' + firstName + ', age: ' + age);
 //alert('Name: ' + firstName + ', age: ' + age);
 //console.log(age);
 //alert(age);
-const lastName = prompt('Enter your last name please');
-alert(firstName + ' ' + lastName);
+//const lastName = prompt('Enter your last name please');
+//alert(firstName + ' ' + lastName);
 
+// 3 Operators
+
+// const currentYear = 2022;
+// const birthYear = 1990;
+// const age = currentYear - birthYear;
+// //alert(age);
+//
+// let a = 10;
+// let b = 5;
+// let money = 30000;
+//
+// let c = 32;
+//c = c + a;
+//c = c - a;
+//c = c * a;
+//c = c / a;
+// c += a;
+// c -= a;
+// c *= a;
+// c /= a;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+//
+// console.log(++money);
+// console.log(--money);
+//
+// console.log(c);
+
+
+// 4 Data types
+
+let name = 'Ihor';
+let age = 31;
+let isProgrammer = true;
+let x
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isProgrammer);
+console.log(typeof x);
