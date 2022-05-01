1d0a9af43b6c786b758ab65bdaa64f97207d97df
IhorShylov <shilov0209@gmail.com>
2022-05-01 23:22:56 +0300

    Number

--- hello_world/app.js
+++ hello_world/app.js
@@ -5,3 +5,7 @@ let pow = 10e3;
 console.log(num);
 console.log(float);
 console.log(pow);
+console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER);
+console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER);
+console.log('MAX_VALUE', Number.MAX_VALUE);
+console.log('MIN_VALUE', Number.MIN_VALUE);
