3d076c9ad513ac818780a371a577f081f557fa9d
IhorShylov <shilov0209@gmail.com>
2022-06-04 21:24:28 +0300

    Math

--- hello_world/app.js
+++ hello_world/app.js
@@ -1,30 +1,57 @@
-// Number
-let num = 42; // integer
-let float = 42.42; // float
-let pow = 10e3;
-console.log(num);
-console.log(float);
-console.log(pow);
-console.log(Math.pow(2, 53) - 1);
-console.log('MAX_SAFE_INTEGER:', Number.MAX_SAFE_INTEGER);
-console.log('MIN_SAFE_INTEGER:', Number.MIN_SAFE_INTEGER);
-console.log('MAX_VALUE:', Number.MAX_VALUE);
-console.log('MIN_VALUE:', Number.MIN_VALUE);
-console.log('POSITIVE_INFINITY:', Number.POSITIVE_INFINITY);
-console.log('NEGATIVE_INFINITY:', Number.NEGATIVE_INFINITY);
-console.log(Number.NaN);
-let weird = 2 / undefined;
-console.log(Number.isNaN(weird));
-console.log(Number.isNaN(42));
-console.log(Number.isFinite(Infinity));
-console.log(Number.isFinite(42));
+// 1 Number
+
+// let num = 42; // integer
+// let float = 42.42; // float
+// let pow = 10e3;
+// console.log(num);
+// console.log(float);
+// console.log(pow);
+// console.log(Math.pow(2, 53) - 1);
+// console.log('MAX_SAFE_INTEGER:', Number.MAX_SAFE_INTEGER);
+// console.log('MIN_SAFE_INTEGER:', Number.MIN_SAFE_INTEGER);
+// console.log('MAX_VALUE:', Number.MAX_VALUE);
+// console.log('MIN_VALUE:', Number.MIN_VALUE);
+// console.log('POSITIVE_INFINITY:', Number.POSITIVE_INFINITY);
+// console.log('NEGATIVE_INFINITY:', Number.NEGATIVE_INFINITY);
+// console.log(Number.NaN);
+// let weird = 2 / undefined;
+// console.log(Number.isNaN(weird));
+// console.log(Number.isNaN(42));
+// console.log(Number.isFinite(Infinity));
+// console.log(Number.isFinite(42));
 
 
-let stringInt = '42';
-let stringFloat = '42.42';
-console.log(stringInt + 2);
-console.log(Number(stringInt) + 2);
-console.log(+stringInt + 2);
-console.log(Number.parseInt(stringInt) + 2);
-console.log(stringFloat + 2);
-console.log(Number.parseFloat(stringFloat) + 2);
+// let stringInt = '42';
+// let stringFloat = '42.42';
+// console.log(stringInt + 2);
+// console.log(Number(stringInt) + 2);
+// console.log(+stringInt + 2);
+// console.log(Number.parseInt(stringInt) + 2);
+// console.log(stringFloat + 2);
+// console.log(Number.parseFloat(stringFloat) + 2);
+
+// console.log(0.4 + 0.2);// 0.6
+// console.log(parseFloat((0.4 + 0.2).toFixed(1)));
+
+// 2 BigInt
+
+// console.log(90071992547409919999999999n - 900719925474099199999999n);
+// console.log(-90071992547409919999999999n);
+// //console.log(-90071992547409919999999999.233424n);// error
+// console.log(parseInt(10n) - 4);
+// console.log(10n - BigInt(4));
+// console.log(5n / 2n);
+
+// 3 Math
+console.log(Math.E);
+console.log(Math.PI);
+
+console.log(Math.sqrt(25));
+console.log(Math.pow(5, 2));
+console.log(Math.abs(-42));
+console.log(Math.max(42,12,57,185,));
+console.log(Math.min(42,12,57,185,));
+console.log(Math.floor(42.9));
+console.log(Math.ceil(42.9));
+
+
