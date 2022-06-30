d3ceccb7ad89fe930a38f299a8cb7871bf908d72
IhorShylov <shilov0209@gmail.com>
2022-06-30 23:32:14 +0300

    String

--- hello_world/app.js
+++ hello_world/app.js
@@ -1,57 +1,29 @@
-// 1 Number
+// const name = 'Ihor';
+// console.log(typeof name);
+// const age = 31;
+//
+// function getAge() {
+//     return age;
+// }
+//
+// //const output = 'Hello my name ' + name + ' and years: ' + age + ' old.';
+// const output = `Hello my name ${name} and years ${getAge()} old.`;
+// const output = `Hello my name ${name} and years ${age < 30 ? 'A' : 'B'} old.`;
+// console.log(output);
 
-// let num = 42; // integer
-// let float = 42.42; // float
-// let pow = 10e3;
-// console.log(num);
-// console.log(float);
-// console.log(pow);
-// console.log(Math.pow(2, 53) - 1);
-// console.log('MAX_SAFE_INTEGER:', Number.MAX_SAFE_INTEGER);
-// console.log('MIN_SAFE_INTEGER:', Number.MIN_SAFE_INTEGER);
-// console.log('MAX_VALUE:', Number.MAX_VALUE);
-// console.log('MIN_VALUE:', Number.MIN_VALUE);
-// console.log('POSITIVE_INFINITY:', Number.POSITIVE_INFINITY);
-// console.log('NEGATIVE_INFINITY:', Number.NEGATIVE_INFINITY);
-// console.log(Number.NaN);
-// let weird = 2 / undefined;
-// console.log(Number.isNaN(weird));
-// console.log(Number.isNaN(42));
-// console.log(Number.isFinite(Infinity));
-// console.log(Number.isFinite(42));
-
-
-// let stringInt = '42';
-// let stringFloat = '42.42';
-// console.log(stringInt + 2);
-// console.log(Number(stringInt) + 2);
-// console.log(+stringInt + 2);
-// console.log(Number.parseInt(stringInt) + 2);
-// console.log(stringFloat + 2);
-// console.log(Number.parseFloat(stringFloat) + 2);
-
-// console.log(0.4 + 0.2);// 0.6
-// console.log(parseFloat((0.4 + 0.2).toFixed(1)));
-
-// 2 BigInt
-
-// console.log(90071992547409919999999999n - 900719925474099199999999n);
-// console.log(-90071992547409919999999999n);
-// //console.log(-90071992547409919999999999.233424n);// error
-// console.log(parseInt(10n) - 4);
-// console.log(10n - BigInt(4));
-// console.log(5n / 2n);
-
-// 3 Math
-console.log(Math.E);
-console.log(Math.PI);
-
-console.log(Math.sqrt(25));
-console.log(Math.pow(5, 2));
-console.log(Math.abs(-42));
-console.log(Math.max(42,12,57,185,));
-console.log(Math.min(42,12,57,185,));
-console.log(Math.floor(42.9));
-console.log(Math.ceil(42.9));
-
-
+// const name = 'Ihor';
+// console.log(name.length);
+// console.log(name.toLocaleUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.charAt(2));
+// console.log(name.indexOf('or'));
+// console.log(name.indexOf('hr'));
+// console.log(name.startsWith('Ih'));
+// console.log(name.endsWith('or!'));
+// console.log(name.repeat(5));
+// const string = '     password     ';
+//
+// console.log(string);
+// console.log(string.trim());
+// console.log(string.trimEnd());
+// console.log(string.trimStart());
